d3ed0109e9226aa2e000aa4e36fe2ccef894143d
ninja6734 <123981615+ninja6734@users.noreply.github.com>
2024-07-29 21:16:54 +0200

    some fixes

--- script.js
+++ script.js
@@ -7,12 +7,9 @@ function initCss(){
         css.href = cssName;
         css.rel = "stylesheet";
 
-        console.log(css)
-
         document.head.appendChild(css);
 }
 
 window.onload = function(){
-    console.log("yay")
     initCss();
 };
