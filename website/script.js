e79b05eef09938c62662f28f827df836a33241b5
Dennis Skurka <de.skurka@gmail.com>
2024-12-29 23:08:12 +0100

    idk what I added

--- website/script.js
+++ website/script.js
@@ -8,13 +8,42 @@ function initCss(){
         css.rel = "stylesheet";
 
         document.head.appendChild(css);
-
-    if(filename == "news.html"){
-        let newsContainer = url.substring(0, url.lastIndexOf("/")) + "/news/exampleNews.html";
-        console.log(fetch(newsContainer));
-    }
 }
 
 window.onload = function(){
     initCss();
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navButtons = document.querySelector('.nav-buttons');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+
+    const updateNavigation = () => {
+    const navbarWidth = document.querySelector('.nav-bar').offsetWidth;
+    const dropButtons = Array.from(dropdownContent.children);
+    let totalWidth = dropdownBtn.offsetWidth;
+    let buttonWidth = [];
+    const buttons = Array.from(navButtons.children);
+    
+    // Move all buttons back to the nav initially
+    buttons.forEach(button => {
+        buttonWidth.push(button.offsetWidth);
+    });
+    console.log(buttonWidth);
+    console.log(buttons);
+
+    for (let i = 0; i < buttons.length; i++) {
+        totalWidth += buttonWidth[i] + 15;
+        if(totalWidth < navbarWidth){
+            navButtons.prepend(buttons[i]);
+        }
+        else {
+            dropdownContent.prepend(buttons[i]);
+        }
+    }
+    };
+    window.addEventListener('resize', updateNavigation);
+    updateNavigation();
+});
